efec0869fc7a41ed3365a070d11032421d42e614
Artem Khvostyk <5hv5art@gmail.com>
2023-05-10 18:52:22 +0200

    init: setup project

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Hangman</h1>
+    </main>
+  );
+};
+
+export default Home;
